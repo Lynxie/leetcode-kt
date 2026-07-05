769d5120345fbb6cc5b4f55992c08495d018406c
LynX <lynx4mf@gmail.com>
2026-07-06 01:46:13 +0300

    12. Integer to Roman - optimization 2

--- src/solutions/IntegerToRoman.kt
+++ src/solutions/IntegerToRoman.kt
@@ -2,51 +2,56 @@ package solutions
 
 class IntegerToRoman {
 
-    val map = arrayOf(
-        1000 to "M",
-        900 to "CM",
-        500 to "D",
-        400 to "CD",
-        100 to "C",
-        90 to "XC",
-        50 to "L",
-        40 to "XL",
-        10 to "X",
-        9 to "IX",
-        5 to "V",
-        4 to "IV",
-        1 to "I"
-    )
+    companion object {
+        val map = arrayOf(
+            1000 to "M",
+            900 to "CM",
+            500 to "D",
+            400 to "CD",
+            100 to "C",
+            90 to "XC",
+            50 to "L",
+            40 to "XL",
+            10 to "X",
+            9 to "IX",
+            5 to "V",
+            4 to "IV",
+            1 to "I"
+        )
+        val result = StringBuilder()
+    }
 
     fun intToRoman(num: Int): String {
-        var result = ""
+        result.setLength(0)
         var numCopy = num
 
         for ((int, romanChar) in map) {
             while (int <= numCopy) {
                 numCopy -= int
-                result += romanChar
+                result.append(romanChar)
             }
         }
 
-        return result
+        return result.toString()
     }
 
     fun intToRoman2(num: Int): String {
-        var result = ""
+        result.setLength(0)
         var numCopy = num
 
         while (numCopy > 0) {
             for ((romanInt, romanChar) in map) {
                 if (romanInt <= numCopy) {
                     numCopy -= romanInt
-                    result += romanChar
+                    result.append(romanChar)
                     break
                 }
             }
         }
 
-        return result
+        return result.toString()
     }
+
+
 
 }
