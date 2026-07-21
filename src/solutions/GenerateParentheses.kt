91e06fb83f86b2633bd59e903edcf2c3408bb7e6
Lynxie <lynx4mf@gmail.com>
2026-07-21 23:01:36 +0300

    22. Generate Parentheses - 2nd try

--- src/solutions/GenerateParentheses.kt
+++ src/solutions/GenerateParentheses.kt
@@ -5,27 +5,25 @@ package solutions
 class GenerateParentheses {
 
     fun generateParenthesis(n: Int): List<String> {
-        var result = mutableListOf<String>("(")
-        var step = 2
-        val iterations = n * 2
+        val r = mutableListOf<String>()
+        generateOptions(r, "", n, 0, 0)
 
-        while (step <= iterations) {
-            val nextResult = mutableListOf<String>()
-            for (r in result) {
-                val opened = r.count { c -> c == '(' }
-                val closed = step - opened - 1
-                if (opened < n) {
-                    nextResult.add("$r(")
-                }
-                if (closed < opened) {
-                    nextResult.add("$r)")
-                }
-            }
-            result = nextResult
-            step++
+        return r
+    }
+
+    fun generateOptions(carry: MutableList<String>, currentString: String, total: Int, opened: Int, closed: Int) {
+        if (total == opened && total == closed) {
+            carry.add(currentString)
+            return
         }
 
-        return result
+        if (opened < total) {
+            generateOptions(carry,"$currentString(", total, opened + 1, closed)
+        }
+
+        if (closed < opened) {
+            generateOptions(carry,"$currentString)", total, opened, closed + 1)
+        }
     }
 
     fun generateParenthesis2(n: Int): List<String> {
